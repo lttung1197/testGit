ed2809117a1ad0cb601ac906bca746520d7bff1a
lttung1197 <lttung1197bt@gmail.com>
2020-03-25 16:05:35 +0700

    fdaf

--- test.c
+++ test.c
@@ -2,18 +2,18 @@ int main(int argc, char const *argv[])
 {
 	int x = 1;
 	x = 2;
-	x = 3; #hihihi
+	x = 3; //hihihi
 	x = 10;
 	
 	x ++;
 
-	#this is new branch
-	#this is new branch too
+	//this is new branch
+	//this is new branch too
 	
 	return 1;
 }
 
 void func(void)
 {
-	#some cool func
+	//some cool func
 }
